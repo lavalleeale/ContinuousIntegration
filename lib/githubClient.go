c1554e9ecbf2f6299bd9d3183b4edbe90b7dbd77
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2023-06-13 21:59:33 -0700

    Allow running without github client credentials

--- lib/githubClient.go
+++ lib/githubClient.go
@@ -16,7 +16,8 @@ func StartGithubClient() {
 	var err error
 	AppID, err = strconv.ParseInt(os.Getenv("GITHUB_APP_ID"), 10, 64)
 	if err != nil {
-		panic(err)
+		log.Println("Running without github client! This should only be done during tests!")
+		return
 	}
 	Itr, err = ghinstallation.NewAppsTransport(http.DefaultTransport, AppID, []byte(os.Getenv("GITHUB_KEY")))
 	if err != nil {
