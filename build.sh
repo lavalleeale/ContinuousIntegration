33c619eac69b34233c6d2db684bef7a89fe53da4
Alex Lavallee <73203142+lavalleeale@users.noreply.github.com>
2024-09-18 16:26:59 -0400

    Add support for multiple arch build

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #! /usr/bin/env bash
 set -e
 
-docker build images/base -t alex95712/base
-docker build . -f Dockerfile.registry -t alex95712/registry-auth
-docker build . -f Dockerfile.ContinuousIntegration -t alex95712/ci
-docker build services/proxy -t alex95712/proxy
-docker push alex95712/base
-docker push alex95712/registry-auth
-docker push alex95712/ci
-docker push alex95712/proxy
+docker buildx build --platform linux/amd64,linux/arm64 images/base -t alex95712/base --push
+docker buildx build --platform linux/amd64,linux/arm64 . -f Dockerfile.registry -t alex95712/registry-auth --push
+docker buildx build --platform linux/amd64,linux/arm64 . -f Dockerfile.ContinuousIntegration -t alex95712/ci --push
+docker buildx build --platform linux/amd64,linux/arm64 services/proxy -t alex95712/proxy --push
